ac10c27a9e5c4edad7c8bd7892d9e419ee35b515
awjae <djwotmd@gmail.com>
2020-05-02 14:00:15 +0900

    query

--- react-router-tutorial/src/pages/About.js
+++ react-router-tutorial/src/pages/About.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
-const About = () => {
-    return (
-        <div>
-            <h2>소개</h2>
-            <p>
-                안녕하세요, 저는 리액트 라우터 입니다.
-            </p>
-        </div>
-    )
+import queryString from 'query-string';
+const About = ({location, match}) => {
+    // return (
+    //     <div>
+    //         <h2>소개</h2>
+    //         <p>
+    //             안녕하세요, 저는 리액트 라우터 입니다.
+    //         </p>
+    //     </div>
+    // )
+    const query = queryString.parse(location.search)
+    console.log(query)
 }
 export default About;
